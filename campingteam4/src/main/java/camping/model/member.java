fecbaa60c9fc75210f70b651eff0b85bbb171de9
ittda <itta841@naver.com>
2022-05-09 19:10:02 +0900

    test

--- campingteam4/src/main/java/camping/model/member.java
+++ campingteam4/src/main/java/camping/model/member.java
@@ -6,15 +6,12 @@ public class member {
 	private String pwd;
 	private String name;
 	private String zip1;
-	private String zip2;
 	private String jumin;
 	private String addr1;
 	private String addr2;
 	private String phone;
 	private String email;
 	private String profile;
-	private String regdate;
-	private int state;
 	private String delcont;
 	private String deldate;
 	
@@ -41,12 +38,6 @@ public class member {
 	}
 	public void setZip1(String zip1) {
 		this.zip1 = zip1;
-	}
-	public String getZip2() {
-		return zip2;
-	}
-	public void setZip2(String zip2) {
-		this.zip2 = zip2;
 	}
 	public String getJumin() {
 		return jumin;
@@ -83,18 +74,6 @@ public class member {
 	}
 	public void setProfile(String profile) {
 		this.profile = profile;
-	}
-	public String getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(String regdate) {
-		this.regdate = regdate;
-	}
-	public int getState() {
-		return state;
-	}
-	public void setState(int state) {
-		this.state = state;
 	}
 	public String getDelcont() {
 		return delcont;
