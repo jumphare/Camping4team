189fdf76034e55fd50a42944a9bc61e78ccfa49d
simjin1212 <89627022+simjin1212@users.noreply.github.com>
2022-05-16 15:08:30 +0900

    124654984

--- campingteam4/src/main/java/camping/dao/replyDAO.java
+++ campingteam4/src/main/java/camping/dao/replyDAO.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import camping.model.review_reply;
-
+@Repository
 public class replyDAO {
 
 	@Autowired
--- campingteam4/src/main/java/camping/service/replyService.java
+++ campingteam4/src/main/java/camping/service/replyService.java
@@ -3,10 +3,11 @@ package camping.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import camping.dao.replyDAO;
 import camping.model.review_reply;
-
+@Service
 public class replyService {
 	@Autowired
 	private replyDAO dao;
